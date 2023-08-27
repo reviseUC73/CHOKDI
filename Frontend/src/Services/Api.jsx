a3e3fa70e6f45378b6ebcc630cdfb9bd574e9cb4
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-08-27 14:35:49 +0700

    add api service for create data

--- Frontend/src/Services/Api.jsx
+++ Frontend/src/Services/Api.jsx
@@ -35,3 +35,56 @@ export const DeleteInformation = async (CarNumber) => {
     return false;
   }
 };
+
+export const CreateInformation = async (data) => {
+  // Define the data to be sent in the request body
+
+  const baseURL = `http://${host_ip}:${port}/create`;
+  var data_format = JSON.stringify(data);
+
+  if (!data.VehicleNumber || !data.CustomerName || !data.InsuranceCompany) {
+    console.log("Missing required field(s)");
+    return false;
+  }
+
+  try {
+    // Send the PUT request
+    const response = await axios.post(baseURL, data_format, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 201) {
+      return true;
+    } else if (response.status === 400) {
+      console.log(response.data);
+    }
+    return false;
+  } catch (e) {
+    console.log("Failed to submit data", e);
+    return false;
+  }
+};
+
+export const EditInformation = async (user_id, data) => {
+  const baseURL = `http://${host_ip}:${port}/edit/${user_id}`;
+  var data_format = JSON.stringify(data);
+  try {
+    // Send the PUT request
+    const response = await axios.post(baseURL, data_format, {
+      headers: {
+        // Overwrite Axios's automatically set Content-Type
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response.status);
+    if (response.status === 200) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    console.log("Failed to fail data", e);
+    return false;
+  }
+};
