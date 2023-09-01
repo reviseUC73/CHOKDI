231f28b9419288d0fc3fdc76fdae361625dfb24c
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-09-01 19:25:37 +0700

    fix bug fetch when edit

--- Frontend/src/Services/EditFormData.jsx
+++ Frontend/src/Services/EditFormData.jsx
@@ -2,29 +2,8 @@ import { CheckDuplicateData, CreateInformation, EditInformation } from "./Api";
 import Swal from "sweetalert2";
 import moment from "moment";
 export const OnSubmitEditFormData = async (e, fromInput) => {
-  // console.log(fromInput);
   e.preventDefault();
-  // console.log("start submit");
-  // const Data = {
-  //   CustomerName: fromInput.CustomerName,
-  //   CustomerAddress: fromInput.CustomerAddress.trim(),
-  //   Brand: fromInput.Brand.trim(),
-  //   Model: fromInput.Model.trim(),
-  //   EngineCapacity: fromInput.EngineCapacity.trim(),
-  //   VehicleNumber: fromInput.VehicleNumber.trim(),
-  //   VehicleManufactureYear: fromInput.VehicleManufactureYear.trim(),
-  //   VehicleBody: fromInput.VehicleBody.trim(),
-  //   VehicleType: fromInput.VehicleType.trim(),
-  //   VehicleCode: fromInput.VehicleCode.trim(),
-  //   InsuranceCompany: fromInput.InsuranceCompany.trim(),
-  //   CoverageType: fromInput.CoverageType.trim(),
-  //   CoverageStartDate: fromInput.CoverageStartDate,
-  //   CoverageEndDate: fromInput.CoverageEndDate,
-  //   PolicyValue: fromInput.PolicyValue.trim(),
-  //   Remark: fromInput.Remark.trim(),
-  //   Mail: "user@gmail.com",
 
-  // };
   try {
     const result = await Swal.fire({
       title: "Are you sure?",
@@ -51,7 +30,7 @@ export const OnSubmitEditFormData = async (e, fromInput) => {
           icon: "success",
           title: `VehicleNumber : ${fromInput.VehicleNumber} <br/>has been Changed`,
           timer: 1500,
-        })
+        }).then(() => window.location.reload());
       } else {
         Swal.fire({
           icon: "error",
