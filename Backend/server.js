96ed29664e3f5ee665ad2ccc23fab85544fcb4a3
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-09-13 02:48:16 +0700

    add api of google oauth user for create account by use token check

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,15 @@
-// impoort
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
+var bodyParser = require("body-parser");
+// import axios from "axios";
+const axios = require("axios");
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
 // const bodyParser = require("body-parser");
 
 require("dotenv").config(); // Load environment variables from .env file
@@ -104,13 +112,7 @@ app.get("/read", (req, res) => {
         return res.status(400).send();
       }
       console.log(result);
-      //  result.map(e)
-      // let new_r = result.map((r) => ({
-      //   ...r,
-      //   CoverageStartDate: ConvertDateTimeFormat(r.CoverageStartDate),
-      //   CoverageEndDate: ConvertDateTimeFormat(r.CoverageEndDate),
 
-      // }));
       return res.status(200).json(result);
     });
   } catch (err) {
@@ -232,6 +234,80 @@ app.post("/check-duplicate", (req, res) => {
     res.json({ duplicate });
   });
 });
+
+// use when user auth by oauth / check email of user has db ?
+app.post("/check-email-used", jsonParser, (req, res) => {
+  const userData = req.body; // we use body-parser to get json from request's body (jsonParser)
+  const { email } = userData; // email = userData.email -> we get string email from json and when you request to server you must send json(body) with key is email
+  // const { mail } = userData; -> when  call api in body req you sent { mail : ....} in body reply
+  // res.json(email);
+  const query = `SELECT * FROM UserAccount WHERE Mail = ?`;
+  db.query(query, [email], (err, result) => {
+    if (err) {
+      console.log("Failed to execute the check email query", err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    const emailUsed = result.length > 0;
+    res.json({ MailUsed: emailUsed });
+    return;
+  });
+});
+
+const G_TokenVerify = async (token) => {
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${token}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          // value: "same-origin", // "same-origin-allow-popups"
+        },
+      }
+    );
+    console.log("response", "call api is successed");
+    return response.status === 200;
+  } catch (err) {
+    // console.log("Error in token decode", err);
+    return false;
+  }
+};
+
+app.post("/google-register", jsonParser, async (req, res) => {
+  const userData = req.body;
+  const { Mail, Password, FirstName, LastName, Role, Token } = userData;
+  const query = `INSERT INTO UserAccount (Mail, Password, FirstName, LastName, Role) VALUES (?,?,?,?,?)`;
+  const isTokenValid = await G_TokenVerify(Token);
+  if (!isTokenValid) {
+    res.status(401).json({ error: "Invalid token" });
+    return;
+  }
+  db.query(
+    query,
+    [Mail, Password, FirstName, LastName, Role],
+    (err, result) => {
+      if (err) {
+        console.log("Failed to execute the register query", err);
+        res.status(500).json({ error: "Internal server error" });
+        return;
+      }
+      console.log("result", result);
+      res.json({
+        message: "Register success",
+        affectedRows: result.affectedRows,
+      });
+    }
+  );
+});
+// POST /login gets urlencoded bodies
+// app.post("/login", urlencodedParser, function (req, res) {
+// const userData = req.body;
+// res.json(userData);
+// res.send( userData);
+// res.json({ message: "Login success" });
+// });
+
 // Listen server
 app.listen(process.env.SERVER_PORT || 3002, () => {
   console.log(`Server is running on port ${process.env.SERVER_PORT || 3002}`);
